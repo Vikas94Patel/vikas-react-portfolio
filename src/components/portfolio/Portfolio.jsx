6a0a9a41cb4e34a592bf28e5c0d0ba7da90a4fd4
Vikas Patel <vikspatel121@gmail.com>
2022-06-20 17:14:40 +0530

    Added new projects in portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import "./portfolio.css";
-import IMG1 from "../../assets/portfolio1.png";
-import IMG2 from "../../assets/portfolio2.png";
-import IMG3 from "../../assets/portfolio3.png";
-import IMG4 from "../../assets/portfolio4.png";
-import IMG5 from "../../assets/portfolio5.png";
-import IMG6 from "../../assets/portfolio6.png";
+import IMG1 from "../../assets/portfolio8.png";
+import IMG2 from "../../assets/portfolio7.png";
+import IMG3 from "../../assets/portfolio1.png";
+import IMG4 from "../../assets/portfolio2.png";
+import IMG5 from "../../assets/portfolio3.png";
+import IMG6 from "../../assets/portfolio4.png";
+import IMG7 from "../../assets/portfolio5.png";
+import IMG8 from "../../assets/portfolio6.png";
+import IMG9 from "../../assets/portfolio9.png";
 
 const data = [
   {
     id: 1,
     image: IMG1,
-    title: "eCommerce Website Shopping Cart",
-    github: "https://github.com/Vikas94Patel/Shopping-Cart",
-    demo: "https://vikas94patel.github.io/Shopping-Cart/",
+    title: "VChat App",
+    github: "https://github.com/Vikas94Patel/VChat",
+    demo: "https://vchats-app.herokuapp.com/",
   },
   {
     id: 2,
-    image: IMG2,
+    image: IMG4,
     title: "Food Ordering App - GomatoEats",
     github: "https://github.com/Vikas94Patel/GomatoEats",
     demo: "https://vikas94patel.github.io/GomatoEats/",
@@ -25,30 +28,51 @@ const data = [
   {
     id: 3,
     image: IMG3,
+    title: "eCommerce Website Shopping Cart",
+    github: "https://github.com/Vikas94Patel/Shopping-Cart",
+    demo: "https://vikas94patel.github.io/Shopping-Cart/",
+  },
+  {
+    id: 4,
+    image: IMG2,
+    title: "React Quiz App",
+    github: "https://github.com/Vikas94Patel/Quiz-App",
+    demo: "https://vikas94patel.github.io/Quiz-App/",
+  },
+  {
+    id: 5,
+    image: IMG5,
     title: "The Dicee Game",
     github: "https://github.com/Vikas94Patel/The-Dicee-Game",
     demo: "https://vikas94patel.github.io/The-Dicee-Game/",
   },
   {
-    id: 4,
-    image: IMG4,
+    id: 6,
+    image: IMG6,
     title: "The Simon Game",
     github: "https://github.com/Vikas94Patel/The-Simon-Game",
     demo: "https://vikas94patel.github.io/The-Simon-Game/",
   },
   {
-    id: 5,
-    image: IMG5,
+    id: 7,
+    image: IMG7,
     title: "McDonald's Website Clone",
     github: "https://github.com/Vikas94Patel/McDonald-s-Clone",
     demo: "https://vikas94patel.github.io/McDonald-s-Clone/",
   },
   {
-    id: 6,
-    image: IMG6,
+    id: 8,
+    image: IMG8,
     title: "Weather App",
     github: "https://github.com/Vikas94Patel/node-weather-app",
     demo: "https://viks-weather-application.herokuapp.com/",
+  },
+  {
+    id: 9,
+    image: IMG9,
+    title: "TinDog App",
+    github: "https://github.com/Vikas94Patel/tinDog-website",
+    demo: "https://vikas94patel.github.io/tinDog-website/",
   },
 ];
 
